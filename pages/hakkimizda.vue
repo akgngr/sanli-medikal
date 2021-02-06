8b247b121d855f22e075f031ae22a8bbe2e3cc53
akgngr <abdulkadirgngr@hotmail.com>
2021-02-06 17:04:07 +0300

    bayi iconlarındaki sorun giderildi.

--- pages/hakkimizda.vue
+++ pages/hakkimizda.vue
@@ -5,17 +5,20 @@
       description='Firmamız 1985 yılında Urfa Medikal adı altında bölgede ilk medikal sektöründe faaliyete girmiş olup 1992 yılında Şanlı Medikal olarak isim değişikliği yaparak yoluna devam etmiştir.'
     />
     <About />
+    <bayiler />
   </div>
 </template>
 
 <script>
 import About from "~/components/about"
 import seo from '~/components/seo'
+import bayiler from '~/components/bayiler'
 export default {
   name: 'hakkimizda',
   components:{
     About,
-    seo
+    seo,
+    bayiler
   },
   mounted() {
     this.$nextTick(() => {
